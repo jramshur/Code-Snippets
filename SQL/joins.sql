80aee3a84f10837bdf93347c0f390ce9db3a1999
John Ramshur <jramshur@users.noreply.github.com>
2018-05-11 12:40:38 -0500

    Create joins.sql

---
+++ SQL/joins.sql
@@ -0,0 +1,11 @@
+
+-- Basic Join
+
+
+-- ANTI JOIN: all values from table1 where not in table2
+-- Source: http://blog.montmere.com/2010/12/08/the-anti-join-all-values-from-table1-where-not-in-table2/
+SELECT
+*
+FROM table1 t1
+LEFT JOIN table2 t2 ON t1.id = t2.id
+WHERE t2.id IS NULL
