238cb19a1eb00c8a09bb00c40e3146292c3bbd94
John Ramshur <jramshur@users.noreply.github.com>
2020-10-28 14:50:18 -0500

    Update dataframe_difference.py

--- python/dataframe_difference.py
+++ python/dataframe_difference.py
@@ -1,3 +1,5 @@
+#show diff between two dataframes
+
 def dataframe_difference(df1, df2, which=None):
     """Find rows which are different between two DataFrames."""
     comparison_df = df1.merge(df2,
@@ -7,7 +9,6 @@ def dataframe_difference(df1, df2, which=None):
         diff_df = comparison_df[comparison_df['_merge'] != 'both']
     else:
         diff_df = comparison_df[comparison_df['_merge'] == which]
-    diff_df.to_csv('data/diff.csv')
     return diff_df
     
     #Source: https://hackingandslacking.com/comparing-rows-between-two-pandas-dataframes-c3b174ff560e
